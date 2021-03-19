17786e9222cb16a921a96738514ca6b40ece63a9
Evan Pratten <ewpratten@gmail.com>
2021-03-19 13:23:26 -0400

    small mode tweaks

--- aprs-bcn/__main__.py
+++ aprs-bcn/__main__.py
@@ -26,6 +26,8 @@ def main() -> int:
         "--latlong", help="Comma-seperated lat and long of the station (defaults to geoip)", default=None)
     ap.add_argument("--message", help="APRS message", default="APRS-BCN")
     ap.add_argument(
+        "--wx-mode", help="Run in WX mode", action="store_true")
+    ap.add_argument(
         "--dry-run", help="Run without actually sending the packet", action="store_true")
     args = ap.parse_args()
 
@@ -37,21 +39,16 @@ def main() -> int:
     else:
         print("Using user-supplied location")
 
+    # Convert the location to a useful format
     lat_fmt = float(location.split(",")[0])
     long_fmt = float(location.split(",")[1])
     lat_ddm = aprslib.util.latitude_to_ddm(lat_fmt)
     long_ddm = aprslib.util.longitude_to_ddm(long_fmt)
 
-    # lat_str = str(round(abs(lat_fmt) * 100, 2)) + \
-    #     ("N" if lat_fmt >= 0 else "S")
-    # long_str = (str(round(abs(long_fmt) * 100, 2)) +
-    #             ("E" if long_fmt >= 0 else "W")).zfill(9)
-
     # Get the location weather
     wx_json = requests.get(
         f"http://wttr.in/{lat_fmt},{long_fmt}?format=j1").json()
     temperature_f = wx_json["current_condition"][0]["temp_F"].zfill(3)
-    temperature_c = wx_json["current_condition"][0]["temp_C"]
     wind_direction = wx_json["current_condition"][0]["winddirDegree"].zfill(3)
     wind_speed = wx_json["current_condition"][0]["windspeedMiles"].zfill(3)
     humidity_percent = wx_json["weather"][0]["hourly"][0]["humidity"].zfill(3)
@@ -61,18 +58,21 @@ def main() -> int:
     packet_header = f"{args.callsign.upper()}{args.ssid}>APRS:!"
     packet_position = f"{lat_ddm}/{long_ddm}"
     packet_wx_data = f"{wind_direction}/{wind_speed}t{temperature_f}h{humidity_percent}b{pressure}"
-    wx_packet = f"{packet_header}{packet_position}_{packet_wx_data}{args.message}" 
-    info_packet = f"{packet_header}{packet_position}{args.symbol}{args.message}" 
+    wx_packet = f"{packet_header}{packet_position}_{packet_wx_data}{args.message}"
+    info_packet = f"{packet_header}{packet_position}{args.symbol}{args.message}"
+
+    if args.wx_mode:
+        packet = wx_packet
+    else:
+        packet = info_packet
 
     # Send
-    print(f"Sending packet: {wx_packet}")
-    # print(f"Sending packet: {info_packet}")
+    print(f"Sending packet: {packet}")
     if not args.dry_run:
         AIS = aprslib.IS(args.callsign, passwd=aprslib.passcode(
             args.callsign), port=14580)
         AIS.connect()
-        AIS.sendall(wx_packet)
-        # AIS.sendall(info_packet)
+        AIS.sendall(packet)
     else:
         print("--dry-run mode enabled. Not actually sending packet")
 
